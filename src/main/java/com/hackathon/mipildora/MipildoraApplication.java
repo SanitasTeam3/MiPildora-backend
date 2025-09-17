11a02017fded22cc75b0bec97c6c810585a01276
VitaPoperechna <vitapoperechna@gmal.com>
2025-09-17 22:18:11 +0200

    first commit

---
+++ src/main/java/com/hackathon/mipildora/MipildoraApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.mipildora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MipildoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MipildoraApplication.class, args);
+	}
+
+}
