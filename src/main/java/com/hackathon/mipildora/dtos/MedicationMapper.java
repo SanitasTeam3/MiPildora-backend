8e0dd3c94224047341922073684fb5e4e8f49d9d
Vita Poperechna <125814047+VitaPoperechna@users.noreply.github.com>
2025-09-18 13:30:48 +0200

    Revert "Revert 5 feature/medication service"

---
+++ src/main/java/com/hackathon/mipildora/dtos/MedicationMapper.java
@@ -0,0 +1,38 @@
+package com.hackathon.mipildora.dtos;
+
+import com.hackathon.mipildora.models.Medication;
+
+public class MedicationMapper {
+    public static Medication dtoToEntity(MedicationRequest request) {
+        if (request == null) return null;
+
+        return Medication.builder()
+                .name(request.name())
+                .dosage(request.dosage())
+                .frequency(request.frequency())
+                .time(request.time())
+                .initDate(request.initDate())
+                .finalDate(request.finalDate())
+                .taken(request.taken() != null ? request.taken() : false)
+                .alert(request.alert() != null ? request.alert() : true)
+                .description(request.description())
+                .build();
+    }
+
+    public static MedicationResponse entityToDto(Medication medication) {
+        if (medication == null) return null;
+
+        return new MedicationResponse(
+                medication.getName(),
+                medication.getDosage(),
+                medication.getFrequency(),
+                medication.getTime(),
+                medication.getInitDate(),
+                medication.getFinalDate(),
+                medication.getTaken(),
+                medication.getAlert(),
+                medication.getCreateDate(),
+                medication.getDescription()
+        );
+    }
+}
