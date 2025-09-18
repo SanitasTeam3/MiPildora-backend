74b435525698900e4dc64babe384b850e90256a5
VitaPoperechna <vitapoperechna@gmal.com>
2025-09-18 12:30:34 +0200

    feat: add data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,20 @@
+INSERT INTO medication (name, dosage, frequency, time, init_date, taken, alert, description, final_date) VALUES
+('Paracetamol', '500mg', 3, '08:00:00', '2025-09-01', false, true, 'Medicamento para bajar la fiebre', '2025-09-10 20:00:00'),
+
+('Ibuprofeno', '200mg', 2, '09:30:00', '2025-09-05', true, true, 'Analgésico y antiinflamatorio', NULL),
+
+('Amoxicilina', '250mg', 3, '07:00:00', '2025-09-10', false, true, 'Antibiótico de amplio espectro', '2025-09-17 07:00:00'),
+
+('Loratadina', '10mg', 1, '08:30:00', '2025-08-15', true, false, 'Antialérgico', NULL),
+
+('Vitamina C', '1000mg', 1, '10:00:00', '2025-09-12', false, true, 'Suplemento inmunológico', '2025-10-12 10:00:00'),
+
+('Omeprazol', '20mg', 1, '06:45:00', '2025-09-01', true, true, 'Para problemas estomacales', NULL),
+
+('Metformina', '500mg', 2, '07:30:00', '2025-07-20', true, true, 'Para controlar la glucosa', NULL),
+
+('Citrato de cafeína', '1 tab', 1, '12:00:00', '2025-09-18', false, true, 'Alivio del dolor de cabeza', NULL),
+
+('Fluconazol', '150mg', 1, '14:00:00', '2025-09-15', false, true, 'Antifúngico oral', '2025-09-16 14:00:00'),
+
+('Analgina', '500mg', 1, '18:30:00', '2025-08-30', true, false, 'Analgésico general', NULL);
